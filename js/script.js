66e3ee661d3e9891989e75e8cc2b9b4dce675693
Matteo0421 <matteo.carola99@gmail.com>
2024-02-27 17:35:27 +0100

    END

--- js/script.js
+++ js/script.js
@@ -9,5 +9,41 @@ const listaSpesa = [
   'olio'
 ];
 
+const cibiEstratti = [];
+
+
+const output = document.getElementById('output')
+const btn = document.getElementById('estrai')
+
 let counter = 0;
 
+btn.addEventListener('click', function(){
+  btn.innerHTML = 'poi';
+  let estrazioneCiboValida = false;
+
+
+  while(!estrazioneCiboValida){
+    const randomNumber = Math.floor(Math.random() * listaSpesa.length)
+    const ciboEstratto = listaSpesa[randomNumber];
+
+
+    estrazioneCiboValida = !cibiEstratti.includes(ciboEstratto);
+
+    if(estrazioneCiboValida){
+      cibiEstratti.push(ciboEstratto)
+      output.innerHTML = ciboEstratto;
+    }
+
+
+    if(cibiEstratti.length === listaSpesa.length && !estrazioneCiboValida){
+      output.innerHTML = 'RIPETI';
+      btn.innerHTML = 'La spesa è finita';
+      console.log(output);
+
+      cibiEstratti.splice(0)
+      estrazioneValida = true;
+    }
+
+  }
+
+})
