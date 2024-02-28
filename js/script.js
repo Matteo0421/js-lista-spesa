59f7238fa6a8fa6ff7b14668e358b2f648f597d5
Matteo0421 <matteo.carola99@gmail.com>
2024-02-28 09:52:19 +0100

    .

--- js/script.js
+++ js/script.js
@@ -9,51 +9,53 @@ const listaSpesa = [
   'olio'
 ];
 
-const cibiEstratti = [];
-
 
 const output = document.getElementById('output')
 const btn = document.getElementById('estrai')
-
 let counter = 0;
 
-btn.addEventListener('click', function(){
-  btn.innerHTML = 'poi';
-  let estrazioneCiboValida = false;
-
-
-  while(!estrazioneCiboValida){
-    const randomNumber = Math.floor(Math.random() * listaSpesa.length)
-    const ciboEstratto = listaSpesa[randomNumber];
-
-
-    estrazioneCiboValida = !cibiEstratti.includes(ciboEstratto);
-
-    if(estrazioneCiboValida){
-      cibiEstratti.push(ciboEstratto)
-      output.innerHTML = ciboEstratto;
-    }
-
-
-    if(cibiEstratti.length === listaSpesa.length && !estrazioneCiboValida){
-      output.innerHTML = 'RIPETI';
-      btn.innerHTML = 'La spesa è finita';
-      console.log(output);
-
-      cibiEstratti.splice(0)
-      estrazioneValida = true;
-    }
-
+btn.addEventListener('click', function () {
+  while(counter < listaSpesa.length){
+    console.log(listaSpesa[counter]);
+    output.innerHTML =listaSpesa[counter];
+  
+    counter++;
   }
-
 })
 
-// btn.addEventListener('click', function () {
-//   while(counter < listaSpesa.length){
-//     console.log(listaSpesa[counter]);
-//     output.innerHTML =listaSpesa[counter];
-  
-//     counter++;
+
+
+// const cibiEstratti = [];
+
+// btn.addEventListener('click', function(){
+//   btn.innerHTML = 'poi';
+//   let estrazioneCiboValida = false;
+
+
+//   while(!estrazioneCiboValida){
+//     const randomNumber = Math.floor(Math.random() * listaSpesa.length)
+//     const ciboEstratto = listaSpesa[randomNumber];
+
+
+//     estrazioneCiboValida = !cibiEstratti.includes(ciboEstratto);
+
+//     if(estrazioneCiboValida){
+//       cibiEstratti.push(ciboEstratto)
+//       output.innerHTML = ciboEstratto;
+//     }
+
+
+//     if(cibiEstratti.length === listaSpesa.length && !estrazioneCiboValida){
+//       output.innerHTML = 'RIPETI';
+//       btn.innerHTML = 'La spesa è finita';
+//       console.log(output);
+
+//       cibiEstratti.splice(0)
+//       estrazioneValida = true;
+//     }
+
 //   }
+
 // })
+
 
